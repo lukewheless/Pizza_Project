35c93518e0d8980a904ac81d8a7f779637e4b30d
Wheless <Luke_Wheless@baylor.edu>
2020-12-03 15:37:39 -0600

    enter

--- myshell.py
+++ myshell.py
@@ -1,6 +1,6 @@
 import os 
 import django
-from pizza.models import Pizza
+from pizza.models import Pizza, Topping
 
 os.environ.setdefalt("DJANGO_SETTINGS_MODULE", "pizzeria.settings")
 django.setup()
@@ -9,10 +9,6 @@ pizza = Pizza.objects.all()
 
 for p in pizza:
     print("Pizza ID:", pizza.id, "Pizza:", pizza)
-
-t = Pizza.objects.get(id=1)
-#print(t.name)
-#print(t.date_added)
 
 topping = t.topping_set.all()
 
--- pizza/models.py
+++ pizza/models.py
@@ -12,14 +12,9 @@ class Topping(models.Model):
     pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
     name = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
-    
-    #allows to set it to toppings when more than one topping is added
-    
-    class Meta:
-        verbose_name_plural = 'toppings'
 
     def _str_(self):
-        return f"{self.name[:50]}..."
+        return self.name
 
 class Comment(models.Model):
     pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
