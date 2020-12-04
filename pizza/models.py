c91a9ad98510ccc55600e89ebb0499a01b822953
Wheless <Luke_Wheless@baylor.edu>
2020-12-04 16:51:04 -0600

    enter

--- pizza/models.py
+++ pizza/models.py
@@ -6,10 +6,10 @@ class Pizza(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
 
     def _str_(self):
-        return self.name
+        return self.name # instance of the class
 
 class Topping(models.Model):
-    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE) # has a fk with Pizza to create relational database
     name = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
@@ -17,7 +17,7 @@ class Topping(models.Model):
         return self.name
 
 class Comment(models.Model):
-    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE) #same as topping
     name = models.CharField(max_length = 200)
     date_added = models.DateTimeField(auto_now_add=True)
     
